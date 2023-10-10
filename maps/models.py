d173009e2bd72fe796cdd49a7987162151e477d0
Karl Ryan <kryan.ie1986@gmail.com>
2023-10-10 15:50:29 +0000

    add first model to database

--- maps/models.py
+++ maps/models.py
@@ -4,4 +4,20 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+TYPE_CHOICE = [
+    ('Strawberries', 'Strawberries'),
+    ('Blackberries', 'Blackberries'),
+    ('Nettles', 'Nettles'),
+]
 
+STATUS = ((0, "Unconfirmed"), (1, "Confirmed"))
+
+class Location (models.Model):
+    type = models.CharField(max_length=20, choices=TYPE_CHOICE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.type
+    
