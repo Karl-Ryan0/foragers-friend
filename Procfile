2b15f88021e5d11cb0afc8a8bb70377ffa8e54f6
Karl Ryan <kryan.ie1986@gmail.com>
2023-10-10 14:48:01 +0000

    initial setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn foragersFriend.wsgi
---
+++ foragersFriend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for foragersFriend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foragersFriend.settings')
+
+application = get_asgi_application()
