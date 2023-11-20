83eefc7c4abd18a5d25faf3082128412e0a6a554
Karl Ryan <kryan.ie1986@gmail.com>
2023-11-20 18:07:56 +0000

    add docstrings to models.py

--- maps/models.py
+++ maps/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 from datetime import datetime
 
-# Create your models here.
 
 class LocationType(models.Model):
+    """
+    Represents a type or category for a location.
+
+    Attributes:
+        name (CharField): The name of the location type.
+        description (TextField): An optional description of the location type.
+    """
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
@@ -14,6 +19,23 @@ class LocationType(models.Model):
 
 
 class Location(models.Model):
+    """
+    Represents a geographical location.
+
+    Attributes:
+        notes (TextField): Descriptive notes or information about the location.
+        type (ForeignKey): The type or category
+        of the location, linked to LocationType.
+        latitude (DecimalField): The latitude of the location.
+        longitude (DecimalField): The longitude of the location.
+        verified (BooleanField): A flag indicating whether
+        the location has been verified.
+        created_on (DateTimeField): The date and time when
+        the location was created.
+        user (ForeignKey): The user who created the location.
+        favorited_by (ManyToManyField): Users who have marked the
+        location as a favorite.
+    """
     notes = models.TextField(null=True)
     type = models.ForeignKey('LocationType', on_delete=models.CASCADE)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
@@ -29,6 +51,18 @@ class Location(models.Model):
 
 
 class ContactMessage(models.Model):
+    """
+    Represents a contact message sent by a user.
+
+    Attributes:
+        name (CharField): The name of the person sending the message.
+        email (EmailField): The email address of the person
+        sending the message.
+        subject (CharField): The subject of the message.
+        message (TextField): The content of the message.
+        created_at (DateTimeField): The date and time when
+        the message was created.
+    """
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=200)
